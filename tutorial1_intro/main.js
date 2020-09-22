1f3c3ce515994a691675e4bdc5a1ad7ffef7761d
Suparna Das <suparna.das@gmail.com>
2020-09-22 18:22:27 -0400

    Js file submitted

--- tutorial1_intro/main.js
+++ tutorial1_intro/main.js
@@ -1,5 +1,5 @@
 // load in csv
-d3.csv("../../data/ClassResults.csv").then(data => {
+d3.csv("../data/ClassResults.csv").then(data => {
     // once the data loads, console log it
     console.log("data", data);
   
